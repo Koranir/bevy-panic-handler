169b3b5c011161a9a648c731b885043087be3aa1
Daniel <101683475+Koranir@users.noreply.github.com>
2023-09-26 08:52:23 +1000

    Custom handlers

--- examples/popup.rs
+++ examples/popup.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(bevy_panic_handler::PanicHandler)
+        .add_plugins(bevy_panic_handler::PanicHandler::default_take_panic())
         .add_systems(Startup, || {
             panic!("Example Error.\nNewlines AOK. 😃\n{} too.", "fmt strings")
         })
--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,42 @@
 //!
 //! On supported platforms (windows, macos, linux) will produce a popup using the `msgbox` crate in addition to writing via `log::error!`, or if `bevy::log::LogPlugin` is not enabled, `stderr`.
 
+use std::sync::Arc;
+
 use bevy::prelude::*;
 
-pub struct PanicHandler;
+pub trait PanicHandleFn: Fn(&std::panic::PanicInfo) + Send + Sync {}
+impl<T: Fn(&std::panic::PanicInfo) + Send + Sync> PanicHandleFn for T {}
+
+#[derive(Default)]
+pub struct PanicHandler {
+    custom_hook: Option<Arc<dyn PanicHandleFn>>,
+}
+impl PanicHandler {
+    #[must_use]
+    pub fn new(panic_handler: impl PanicHandleFn + 'static) -> Self {
+        Self {
+            custom_hook: Some(Arc::new(panic_handler)),
+        }
+    }
+
+    #[must_use]
+    pub fn default_take_panic() -> Self {
+        Self {
+            custom_hook: Some(Arc::new(std::panic::take_hook()))
+        }
+    }
+}
 
 impl Plugin for PanicHandler {
-    fn build(&self, app: &mut App) {
-        let use_log = app.is_plugin_added::<bevy::log::LogPlugin>();
+    fn build(&self, _: &mut App) {
+        let custom_hook = self
+            .custom_hook
+            .as_ref().cloned()
+            .unwrap_or_else(|| Arc::new(|_| {}));
         std::panic::set_hook(Box::new(move |info| {
-            let info = format!(
-                "Unhandled panic @ {}:\n{}",
+            let info_string = format!(
+                "Unhandled panic! @ {}:\n{}",
                 info.location()
                     .map_or("Unknown Location".to_owned(), ToString::to_string),
                 info.payload().downcast_ref::<String>().unwrap_or(
@@ -19,16 +45,18 @@ impl Plugin for PanicHandler {
                 )
             );
 
-            if use_log {
-                // Known limitations: Logging in tests prints to stdout immediately.
-                bevy::log::error!("{info}");
-            } else {
-                eprintln!("{info}");
-            }
+            // Known limitations: Logging in tests prints to stdout immediately.
+            // This will print duplicate messages to stdout if the default panic hook is being used & env_logger is initialized.
+            bevy::log::error!("{info_string}");
 
-            // Dont interrupt test execution with a popup, and dont try on unsupported platforms.
-            #[cfg(all(not(test), any(target_os = "windows", target_os = "macos", target_os = "linux")))]
-            { _ = msgbox::create("Fatal Error", &info, msgbox::IconType::Error); }
+            // Don't interrupt test execution with a popup, and dont try on unsupported platforms.
+            #[cfg(all(
+                not(test),
+                any(target_os = "windows", target_os = "macos", target_os = "linux")
+            ))]
+            { _ = msgbox::create("Fatal Error", &info_string, msgbox::IconType::Error); }
+
+            custom_hook(info);
         }));
     }
 }
